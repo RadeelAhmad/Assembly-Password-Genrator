137b57c2f844e9a8154d7c74daf574911eb9d58e
Radeel Ahmad <160111777+RadeelAhmad@users.noreply.github.com>
2024-07-13 03:08:28 +0500

    Update code.asm

--- code.asm
+++ code.asm
@@ -1,115 +1,181 @@
 INCLUDE Irvine32.inc
-
 .data
-    password BYTE 50 DUP(?)
-    passwordLength DWORD ?
-    passwordComplexity DWORD ?
-    uppercase BYTE 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lowercase BYTE 'abcdefghijklmnopqrstuvwxyz'
-    numbers BYTE '0123456789'
-    specialChars BYTE '!@#$%^&*()_+-={}:<>?'
-    allChars BYTE 0
-
+password BYTE 50 DUP(?)
+passwordLength DWORD ?
+passwordComplexity DWORD ?
+uppercase BYTE 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 0
+lowercase BYTE 'abcdefghijklmnopqrstuvwxyz', 0
+numbers BYTE '0123456789', 0
+specialChars BYTE '!@#$%^&*()_+-={}:<>?', 0
+sprompt BYTE "Enter Desired Password Length: ", 0
+smenu BYTE " --------- Main Menu ------------", 0
+smenu1 BYTE "1. Weak Password (lowercase + length fewer than 8)", 0
+smenu2 BYTE "2. Average Password (mixedcase + numbers + length 8 to 12)", 0
+smenu3 BYTE "3. Strong Password (mixedcase + numbers + special characters + length greater than 12)", 0
+soption BYTE "Choose Complexity (1-3): ", 0
+spassword BYTE "Password Will be: ", 0
+serror BYTE "------------ Invalid Length --------- ", 0
 .code
 start PROC
-    call ReadInt
-    mov passwordLength, eax
-
+    mov edx, OFFSET smenu
+    call WriteString
+    call Crlf
+    mov edx, OFFSET smenu1
+    call WriteString
+    call Crlf
+    mov edx, OFFSET smenu2
+    call WriteString
+    call Crlf
+    mov edx, OFFSET smenu3
+    call WriteString
+    call Crlf
+    call Crlf
+    mov edx, OFFSET soption
+    call WriteString
     call ReadInt
     mov passwordComplexity, eax
+    mov edx, OFFSET sprompt
+    call WriteString
+    call ReadInt
+    mov passwordLength, eax
+    ; Validate password length based on complexity
+    mov eax, passwordComplexity
+    cmp eax, 1
+    je validateWeak
+    cmp eax, 2
+    je validateAverage
+    cmp eax, 3
+    je validateStrong
+    jmp invalidOption
 
-    mov esi, offset password
+validateWeak:
+    cmp passwordLength, 8
+    jb generate
+    jmp invalidLength
+
+validateAverage:
+    cmp passwordLength, 8
+    jb invalidLength
+    cmp passwordLength, 12
+    jbe generate
+    jmp invalidLength
+
+validateStrong:
+    cmp passwordLength, 12
+    jb invalidLength
+    jmp generate
+
+invalidLength:
+    call Crlf
+    mov edx, OFFSET serror
+    call WriteString
+    call Crlf
+    invoke ExitProcess, 0
+
+invalidOption:
+    call Crlf
+    mov edx, OFFSET serror
+    call WriteString
+    call Crlf
+    invoke ExitProcess, 0
+
+generate:
+    mov esi, OFFSET password
     mov ecx, passwordLength
 generatePassword:
     call GenerateRandomChar
     mov [esi], al
     inc esi
     loop generatePassword
-
-    mov byte ptr [esi], 0 ; Null-terminate the string
-
-    mov edx, offset password
+    mov byte ptr [esi], 0
+    call Crlf
+    mov edx, OFFSET spassword
+    call WriteString
+    mov edx, OFFSET password
     call WriteString
     call Crlf
-
     invoke ExitProcess, 0
+
 start ENDP
 
 GenerateRandomChar PROC
     mov eax, passwordComplexity
     cmp eax, 1
-    je uppercaseOnly
+    je generateWeakChar
     cmp eax, 2
-    je mixedCase
+    je generateAverageChar
     cmp eax, 3
-    je mixedCaseNumbers
-    cmp eax, 4
-    je allCharacters
-    jmp GenerateRandomChar ; add this to avoid infinite loop
-
-uppercaseOnly:
-    mov eax, 26
-    call RandomRange
-    add eax, offset uppercase
-    mov al, [eax]
+    je generateStrongChar
     ret
 
-mixedCase:
-    mov eax, 52
+generateWeakChar:
+    mov eax, 36
     call RandomRange
     cmp eax, 26
-    jl uppercaseOnlyProc
+    jl weakLowercase
     sub eax, 26
-    add eax, offset lowercase
+    add eax, OFFSET numbers
     mov al, [eax]
     ret
 
-mixedCaseNumbers:
+weakLowercase:
+    add eax, OFFSET lowercase
+    mov al, [eax]
+    ret
+
+generateAverageChar:
     mov eax, 62
     call RandomRange
     cmp eax, 26
-    jl uppercaseOnlyProc
+    jl averageUppercase
     cmp eax, 52
-    jl lowercaseOnlyProc
+    jl averageLowercase
     sub eax, 52
-    add eax, offset numbers
+    add eax, OFFSET numbers
     mov al, [eax]
     ret
 
-allCharacters:
+averageUppercase:
+    add eax, OFFSET uppercase
+    mov al, [eax]
+    ret
+
+averageLowercase:
+    sub eax, 26
+    add eax, OFFSET lowercase
+    mov al, [eax]
+    ret
+
+generateStrongChar:
     mov eax, 94
     call RandomRange
     cmp eax, 26
-    jl uppercaseOnlyProc
+    jl strongUppercase
     cmp eax, 52
-    jl lowercaseOnlyProc
+    jl strongLowercase
     cmp eax, 62
-    jl numbersOnlyProc
+    jl strongNumbers
     sub eax, 62
-    add eax, offset specialChars
+    add eax, OFFSET specialChars
     mov al, [eax]
     ret
 
-uppercaseOnlyProc:
-    mov eax, 26
-    call RandomRange
-    add eax, offset uppercase
+strongUppercase:
+    add eax, OFFSET uppercase
     mov al, [eax]
     ret
 
-lowercaseOnlyProc:
-    mov eax, 26
-    call RandomRange
-    add eax, offset lowercase
+strongLowercase:
+    sub eax, 26
+    add eax, OFFSET lowercase
     mov al, [eax]
     ret
 
-numbersOnlyProc:
-    mov eax, 10
-    call RandomRange
-    add eax, offset numbers
+strongNumbers:
+    sub eax, 52
+    add eax, OFFSET numbers
     mov al, [eax]
     ret
+
 GenerateRandomChar ENDP
-
 END start
